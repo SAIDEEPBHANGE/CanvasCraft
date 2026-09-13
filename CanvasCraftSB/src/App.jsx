f6479ddc1f3e17a3af973051ffcd31d970ee76a7
SAIDEEPBHANGE <saideepbhange11022002@gmail.com>
2026-09-13 13:25:05 +0530

    Tailwind & Lucide Icons are Added

--- CanvasCraftSB/src/App.jsx
+++ CanvasCraftSB/src/App.jsx
@@ -1,5 +1,11 @@
+import { Paintbrush } from "lucide-react";
 function App() {
-  return <>Canvas Craft Projects</>;
+  return (
+    <>
+      <Paintbrush />
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
